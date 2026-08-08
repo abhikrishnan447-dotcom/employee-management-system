042da1eaccd0c5834488fc2efb2a87b6c5c8791b
abhikrishnan447-dotcom <abhikrishnan447@gmail.com>
2026-08-08 12:08:19 +0530

    Add registration department endpoint

--- work_management/work_manage/work_manage_app/urls.py
+++ work_management/work_manage/work_manage_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("home/", views.home, name="home"),
     path("register/", views.register, name="register"),
+    path("register/departments/", views.registration_departments, name="registration_departments"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout, name="logout"),
     path("dashboard/", views.dashboard, name="dashboard"),
