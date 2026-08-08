d38e7c37af9f8c73d26da10c4d46efa8f9d1f653
abhikrishnan447-dotcom <abhikrishnan447@gmail.com>
2026-08-08 18:34:58 +0530

    Add dynamic admin notification and message counters

--- work_management/work_manage/work_manage/settings.py
+++ work_management/work_manage/work_manage/settings.py
@@ -40,6 +40,7 @@ TEMPLATES = [{
         "django.template.context_processors.request",
         "django.contrib.auth.context_processors.auth",
         "django.contrib.messages.context_processors.messages",
+        "work_manage_app.context_processors.admin_badges",
     ]},
 }]
 WSGI_APPLICATION = "work_manage.wsgi.application"
