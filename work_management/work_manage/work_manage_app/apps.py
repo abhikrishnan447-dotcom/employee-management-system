ca39fcc832c4bd1d9c1a8c90c99ffc12d50acfc4
abhikrishnan447-dotcom <abhikrishnan447@gmail.com>
2026-08-09 17:41:04 +0530

    Add clear app configuration section comment

--- work_management/work_manage/work_manage_app/apps.py
+++ work_management/work_manage/work_manage_app/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
 
 
+# ==============================
+# WORK MANAGE APP CONFIGURATION
+# ==============================
 class WorkManageAppConfig(AppConfig):
     name = 'work_manage_app'
